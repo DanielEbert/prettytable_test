209da011c68c2857239da8c8a6f4a1d286794310
Daniel Ebert <iuq@web.de>
2023-07-01 12:51:51 -0700

    use proper input format

--- main.py
+++ main.py
@@ -3,6 +3,9 @@ from __future__ import annotations
 import prettytable
 
 from collections import defaultdict
+from collections import Counter
+import dataclasses
+import json
 
 old_results = {
     'CCP': {
@@ -32,38 +35,128 @@ new_results = {
 }
 
 
-def accumulate_results(single_results: list[dict[str, dict[str, dict[str, int]]]]) -> defaultdict[defaultdict[int]]:
-    results = defaultdict(lambda: defaultdict(lambda: defaultdict(int)))
-    for result in single_results:
-        for subsys_name, subsys_results in result.items():
-            for runnable_name, runnable_results in subsys_results.items():
-                for finding_type, finding_count in runnable_results.items():
-                    results[subsys_name][runnable_name][finding_type] += finding_count
-    return results
+findings = {
+    'CCP': {
+        'RbpCcpOcp': [
+            {
+                'epoch_timestamp': 1688227256,
+                'timestamp': '01.07.2023 18:01:33',   # use same name as folder here
+                'branch': 'develop',
+                'commit': 'b5d7664',
+                'commit_timestamp': '24.06.2023 12:05:17',
+                'bug_type': 'Divide by Zero',
+                'artifactory_folder': '2023_07_01_19_39_22/findings/crashes/0'
+            },
+        ],
+        'RbpCcpOoc': [
+            {
+                'epoch_timestamp': 1688227256,
+                'timestamp': '01.07.2023 18:01:33',   # use same name as folder here
+                'branch': 'develop',
+                'commit': 'b5d7664',
+                'commit_timestamp': '24.06.2023 12:05:17',
+                'bug_type': 'Divide by Zero',
+                'artifactory_folder': '2023_07_01_19_39_22/findings/crashes/0'
+            },
+        ]
+    },
+    'PSD': {
+        'XYZ': [
+            {
+                'epoch_timestamp': 1688227256,
+                'timestamp': '01.07.2023 18:01:33',   # use same name as folder here
+                'branch': 'develop',
+                'commit': 'b5d7664',
+                'commit_timestamp': '24.06.2023 12:05:17',
+                'bug_type': 'Divide by Zero',
+                'artifactory_folder': '2023_07_01_19_39_22/findings/crashes/0'
+            }
+        ]
+    }
+}
 
-def create_table(results: defaultdict[defaultdict[int]]) -> str:
+@dataclasses.dataclass
+class Finding:
+    epoch_timestamp: int
+    timestamp: str
+    branch: str
+    commit: str
+    commit_timestamp: str
+    bug_type: str
+    artifactory_folder: str
+
+# subsys and runnable known
+f1 = Finding(1688227257, '01.07.2023 18:01:33', 'develop', 'b5d7664', '24.06.2023 12:05:17', 'Divide by Zero', '2023_07_01_19_39_22/findings/crashes/1')
+f2 = Finding(1688227259, '01.07.2023 18:02:33', 'develop', 'b5d7664', '24.06.2023 12:05:17', 'Integer Overflow', '2023_07_01_19_39_22/findings/crashes/2')
+f3 = Finding(1688227259, '01.07.2023 18:02:33', 'develop', 'b5d7664', '24.06.2023 12:05:17', 'Divide by Zero', '2023_07_01_19_39_22/findings/crashes/3')
+
+def add_findings(findings, subsys, runnable, new_findings):
+    for finding in new_findings:
+        if subsys not in findings:
+            findings[subsys] = {}
+        if runnable not in findings[subsys]:
+            findings[subsys][runnable] = []
+        findings[subsys][runnable].insert(0, dataclasses.asdict(finding))
+
+add_findings(findings, 'CCP', 'RbpCcpOcp', [f1, f2, f3]) 
+print(json.dumps(findings, indent=2))
+
+def create_findings_table(findings: dict[str, dict[str, list[dict[str, str | int]]]]) -> str:
+    t = prettytable.PrettyTable()
+    t.field_names = ['Subsystem', 'Runnable', 'Detection Timestamp', 'Finding Type', 'Artifactory Folder', 'Branch', 'Commit']
+    t.align = 'l'
+
+    for subsys_name, subsys_findings in findings.items():
+        subsys_column = subsys_name
+        for runnable_name, runnable_findings in subsys_findings.items():
+            previous_column = defaultdict(lambda: None)
+            for i, finding in enumerate(runnable_findings):
+                runnable_column = runnable_name if i == 0 else ''
+                divide = True if len(runnable_findings) - 1 == i else False
+
+                column_names = ['timestamp', 'bug_type', 'artifactory_folder', 'branch', 'commit']
+                columns = []
+
+                for name in column_names:
+                    if name not in finding:
+                        # log.warn...
+                        columns.append('Unknown')
+                        previous_column[name] = None
+                        continue
+                    if finding[name] != previous_column[name] or name == 'bug_type':
+                        columns.append(finding[name])
+                    else:
+                        columns.append('')
+                    previous_column[name] = finding[name]
+
+                t.add_row([subsys_column, runnable_column] + columns, divider=divide)
+
+                subsys_column = ''
+
+    return t.get_string()
+
+def create_finding_count_table(findings: dict[str, dict[str, list[dict[str, str | int]]]]) -> str:
     t = prettytable.PrettyTable()
     t.field_names = ['Subsystem', 'Runnable', 'Finding Type', 'Count']
     t.align = 'l'
     t.align['Count'] = 'c'
 
-    for subsys_name, subsys_results in results.items():
+    for subsys_name, subsys_findings in findings.items():
         subsys_column = subsys_name
-        for runnable_name, runnable_results in subsys_results.items():
-            findings_count = 0
-            for i, finding_type in enumerate(runnable_results):
+        for runnable_name, runnable_findings in subsys_findings.items():
+            finding_counts = Counter([f['bug_type'] for f in runnable_findings]).most_common()
+
+            for i, (finding, count) in enumerate(finding_counts):
                 runnable_column = runnable_name if i == 0 else ''
-                divide = True if len(runnable_results) == 1 else False
+                divide = True if len(finding_counts) == 1 else False
 
-                t.add_row([subsys_column, runnable_column, finding_type, runnable_results[finding_type]], divider=divide)
-
-                findings_count += runnable_results[finding_type]
+                t.add_row([subsys_column, runnable_column, finding, count], divider=divide)
                 subsys_column = ''
 
-            if len(runnable_results) > 1:
+            if len(finding_counts) > 1:
                 # Print Summary Row
                 t.add_row(['', '', '', '-----'])
-                t.add_row(['', '', '', findings_count], divider=True)
+                t.add_row(['', '', '', len(runnable_findings)], divider=True)
 
     return t.get_string()
 
@@ -84,9 +177,9 @@ def postprocess_table(table: str) -> str:
 
     return '\n'.join(rows)
 
-results = accumulate_results([old_results, new_results])
+findings_table = postprocess_table(create_findings_table(findings))
+print(findings_table)
 
-table = postprocess_table(create_table(results))
-
-print(table)
+count_table = postprocess_table(create_finding_count_table(findings))
+print(count_table)
 
